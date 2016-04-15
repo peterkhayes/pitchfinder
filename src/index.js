624dbbd309cec94927c71536943ebbc8ffbed6ba
Peter Hayes <peter.k.hayes@gmail.com>
2016-04-15 09:09:34 -0700

    remove removed files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const AMDF = require("./detectors/amdf");
 const YIN = require("./detectors/yin");
 const DynamicWavelet = require("./detectors/dynamic_wavelet");
-// const FastYIN = require("./detectors/fast_yin");
-// const Macleod = require("./detectors/macleod");
 
 const frequencies = require("./tools/frequencies");
-const notes = require("./tools/notes");
 
 module.exports = {
   AMDF,
@@ -13,5 +10,4 @@ module.exports = {
   DynamicWavelet,
 
   frequencies,
-  notes,
 };
