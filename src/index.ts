f0b693b0acf60884cd3f6138a0327aa135f0817a
Kurt Preston <github@kurtpreston.com>
2020-05-24 10:29:18 -0500

    Exporting PitchDetector types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { AMDF, AMDFConfig } from './detectors/amdf';
 import { ACF2PLUS, ACF2Config } from './detectors/acf2plus';
 import { DynamicWavelet, DynamicWaveletConfig } from './detectors/dynamic_wavelet';
 import { Macleod, MacleodConfig } from './detectors/macleod';
+import {
+  PitchDetector,
+  ProbabalisticPitchDetector,
+  ProbabilityPitch,
+} from './detectors/types';
 
 import { frequencies } from './tools/frequencies';
 
@@ -17,6 +22,10 @@ export {
   DynamicWaveletConfig,
   Macleod,
   MacleodConfig,
+  frequencies,
+  PitchDetector,
+  ProbabalisticPitchDetector,
+  ProbabilityPitch,
 };
 
 export default {
